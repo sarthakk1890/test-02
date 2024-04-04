0ac91ee42b4ad1b7b1ccb4009a11d2d22e3b9d48
sarthakk1890 <144singhsarthak@gmail.com>
2024-04-04 21:19:29 +0530

    Added config folder

---
+++ backend/config/firebase.js
@@ -0,0 +1,15 @@
+
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuGepNO3ZJNeArwbnWdOWtVBsfvwSM9rU",
+  authDomain: "inventory-db598.firebaseapp.com",
+  projectId: "inventory-db598",
+  storageBucket: "inventory-db598.appspot.com",
+  messagingSenderId: "1097755056892",
+  appId: "1:1097755056892:web:ee4609fedeb5617128d579"
+};
+
+const app = initializeApp(firebaseConfig);
